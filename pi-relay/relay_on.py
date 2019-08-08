de48bc9a08449d3312003e9fc52a7275ea01e71c
Sebastian King <sebastian.king@btinternet.com>
2019-08-08 02:07:33 +0100

    add relay, serial expansion and xbox controller code

---
+++ pi-relay/relay_on.py
@@ -0,0 +1,7 @@
+import RPi.GPIO as GPIO   # Import the GPIO library.
+import time               # Import time library
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(21, GPIO.OUT)
+GPIO.output(21, GPIO.LOW)
